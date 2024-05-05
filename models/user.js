c9b51a9bf527efb714f4b3129c61e644d774f09c
Vivek ghoshi <the.vivekghoshi@gmail.com>
2024-05-05 12:55:16 +0530

    edit and delete feature complete

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb://127.0.0.1:27017/userData");
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required: true
+    },
+    username:{
+        type:String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    }
+});
+
+
+module.exports = mongoose.model('user', userSchema);
